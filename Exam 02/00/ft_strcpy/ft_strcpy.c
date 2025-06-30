8b290a43c30aee8a1151c5e9fea26248e9440b77
herib9 <157405740+herib9@users.noreply.github.com>
2025-06-30 14:46:14 +0200

    Add files via upload

--- Exam 02/00/ft_strcpy/ft_strcpy.c
+++ Exam 02/00/ft_strcpy/ft_strcpy.c
@@ -1,10 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
 char    *ft_strcpy(char *s1, char *s2)
 {
-	while (*s1)
+	int	i = 0;
+	while (s2[i])
 	{
-		*s1 = *s2;
-		s1++;
-		s2++;
+		s1[i] = s2[i];
+		i++;
 	}
-	return (s2);
+	s1[i] = '\0';
+	return (s1);
+}
+
+int	main()
+{
+	char	s1[59];
+	char	*s2 = "hola";
+	printf("%s\n", ft_strcpy(s1, s2));
+	printf("%s\n", strcpy(s1, s2));
 }
