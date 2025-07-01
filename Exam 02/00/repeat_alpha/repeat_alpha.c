92b8bc4149e3cbc69d48e201b882e588b004728b
herib9 <157405740+herib9@users.noreply.github.com>
2025-07-01 19:46:05 +0200

    Update repeat_alpha.c

--- Exam 02/00/repeat_alpha/repeat_alpha.c
+++ Exam 02/00/repeat_alpha/repeat_alpha.c
@@ -28,25 +28,3 @@ int	main(int argc, char **argv)
 		repeat_alpha(argv[1]);
 	write(1, "\n", 1);
 }
-
-
-/*void	repeat_alpha(char *c)
-{
-	int repeat = 0;
-
-	while (*c)
-	{
-		if (*c >= 65 && *c <= 90)
-			repeat = *c - 64;
-		else if (*c >= 97 && *c <= 122)
-			repeat = *c - 96;
-		else
-			repeat = 1;
-		while (repeat > 0)
-		{
-			write(1, c, 1);
-			repeat--;
-		}
-		c++;
-	}
-}*/
