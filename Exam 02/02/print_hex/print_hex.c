059dacf5492e75024651d955286de9ff1dfae48a
herib9 <157405740+herib9@users.noreply.github.com>
2025-07-05 20:55:35 +0200

    Update print_hex.c

--- Exam 02/02/print_hex/print_hex.c
+++ Exam 02/02/print_hex/print_hex.c
@@ -26,8 +26,8 @@ int	main(int argc, char **argv)
 	if (argc == 2)
 	{
 		print_hex(ft_atoi(argv[1]));
-		write(1, "\n", 1);
-		print_hex(atoi(argv[1]));
+		//write(1, "\n", 1);
+		//print_hex(atoi(argv[1]));
 	}
 	write(1, "\n", 1);
 	return (0);
