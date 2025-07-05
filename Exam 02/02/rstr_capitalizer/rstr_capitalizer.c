0894983a55b32597a3ee418d7e43a6f5ef2962cb
herib9 <157405740+herib9@users.noreply.github.com>
2025-07-05 02:50:09 +0200

    Update rstr_capitalizer.c

--- Exam 02/02/rstr_capitalizer/rstr_capitalizer.c
+++ Exam 02/02/rstr_capitalizer/rstr_capitalizer.c
@@ -8,6 +8,8 @@ void    rcap(char *s)
     {
         if ((s[i] >= 'a' && s[i] <= 'z') && (s[i + 1] <= 32 || s[i + 1] == '\0'))
 		s[i] -= 32;
+	else if ((s[i] >= 'A' && s[i] <= 'z') && (s[i + 1] <= 32 || s[i + 1] == '\0'))
+		i = i;
 	else if (s[i] >= 'A' && s[i] <= 'Z')
 		s[i] += 32;
         write(1, &s[i], 1);
