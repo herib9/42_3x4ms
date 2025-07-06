f83f4887a6dc652a74548350d5f5fdb2f884381d
herib9 <157405740+herib9@users.noreply.github.com>
2025-07-06 02:39:03 +0200

    Update lcm.c

--- Exam 02/02/lcm2/lcm.c
+++ Exam 02/02/lcm2/lcm.c
@@ -1,38 +1,30 @@
 #include <stdio.h>
 
-int	euclides(int a, int b)
+unsigned int    lcm(unsigned int a, unsigned int b)
 {
-	int	temp;
-	while (b != 0)
-	{
-		temp = b;
-		b = a % b;
-		a = temp;
-	}
-	return (a);
+        int     temp;
+        int     r = a * b;
+        
+        if ((int)a < 0 || (int)b < 0)
+                return (0);
+        if (a == 0 || b == 0)
+                return (0);
+        while(b != 0)   
+        {       
+                temp = b;
+                b = a % b;
+                a = temp;
+        }       
+        return (r / a);
+}       
+
+int     main(void)
+{       
+        printf("%i\n", lcm(-1, 2932));
+        printf("%i\n", lcm(3, 5));
 }
-
-unsigned int	lcm(unsigned int a, unsigned int b)
-{
-	int	res;
-
-	if ((int)a < 0 || (int)b < 0)
-		return(0);
-	res = 0;
-	res = (a / euclides(a, b)) * b;
-	return(res);
-}
-
-/*int	main(void)
-{
-	int	n1 = 0;
-	int	n2 = 1;
-	printf("%i\n", lcm(n1, n2));
-}*/
 
 //(lcm)ft_minimo_comun_multiplo(a, b) = (ft_absolute)ft_valor_absoluto(a * b) / (hcf)ft_maximo_comun_divisor(a, b)
-
-
 //lcm(a, b) = (|a * b|) / (hcf(a, b))
 //producto = a * b
 
