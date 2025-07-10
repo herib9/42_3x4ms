3a68cc8dfe76593614a6f36339b3774a0560ac78
herib9 <157405740+herib9@users.noreply.github.com>
2025-07-10 22:02:35 +0200

    Add files via upload

---
+++ Exam 02/00/rev_print/rev_print_update.c
@@ -0,0 +1,30 @@
+char	*rev(char *s)
+{
+	int	i = 0;
+	int	len = 0;
+	int	temp;
+	
+	while (s[len])
+		len++;
+	len--;
+	while (i < len)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+		i++;
+		len--;
+	}
+	return (s);
+}
+
+int main(void)
+{
+	rev_print("rainbow dash");
+	write(1, "\n", 1);
+	rev_print("Ponies are awesome");
+	write(1, "\n", 1);
+	rev_print("");
+	write(1, "\n", 1);
+	return (0);
+}
