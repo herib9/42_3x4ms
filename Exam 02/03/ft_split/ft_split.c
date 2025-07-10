2aa93443d7597c2ca301d6d79d01eb64c8027c00
herib9 <157405740+herib9@users.noreply.github.com>
2025-07-10 22:30:32 +0200

    Update ft_split.c

--- Exam 02/03/ft_split/ft_split.c
+++ Exam 02/03/ft_split/ft_split.c
@@ -1,3 +1,33 @@
+#include <stdlib.h>
+
+char	**ft_split(char *str)
+{
+	char **split = malloc (sizeof(char *) * 10000);
+	int	i = 0;
+	int	j = 0;
+
+	while (*str)
+	{
+		split[i] = malloc (sizeof(char) * 10000);
+		j = 0;
+		while (*str < 33 && *str != '\0')
+			str++;
+		if (*str == '\0')
+			break ;
+		while (*str > 33)
+		{
+			split[i][j] = *str;
+			j++;
+			str++;
+		}
+		split[i][j] = '\0';
+		i++;
+	}
+	split[i] = NULL;
+	return (split);
+}
+
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -49,83 +79,3 @@ int	main(int ac, char **av)
 	}
 	return (0);
 }
-
-
-/*int	ft_separate(char c)
-	return (c == ' ' || c == '\t' || c == '\n');
-
-int	ft_count(char *s)
-{
-	int	i;
-	int	count;
-
-	i = 0;
-	count = 0;
-	while(s[i])
-	{
-		while (s[i] && ft_separate(s[i]))
-			i++;
-		if (s[i])
-		{
-			count++;
-			while (s[i] && !ft_separate(s[i]))
-				i++;
-		}
-	}
-	return (count);
-}	
-
-int	ft_word_len(char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] && !ft_separate(s[i])
-		i++;
-	return (i);
-}
-
-char	*ft_copy_word(char *s)
-{
-	int	i;
-	char	*str;
-	int	len;
-
-	i = 0;
-	len = ft_word_len(s);
-	str = malloc(sizeof(char) * len + 1);
-	if (!str)
-		return NULL;
-	while (i < len)
-	{
-		str[i] = s[i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
-
-char	**ft_split(char *s)
-{
-	int	i;
-	char **split;
-
-	i = 0;
-	split = malloc(sizeof(char *) * (ft_count[s] + 1));
-	if (!split)
-		return NULL;
-	while (*s)
-	{
-		while (*s && ft_separate(*s))
-			s++;
-		if (*s)
-		{
-			split[i] = ft_copy_word(s);
-			i++;
-			while (*s && !ft_separate(*s))
-				s++;
-		}
-	}
-	split[i] = NULL;
-	return (split);
-}*/
