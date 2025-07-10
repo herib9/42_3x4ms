4c561bd83d61eadbbdcaeff563d2be15ae5f1e99
herib9 <157405740+herib9@users.noreply.github.com>
2025-07-10 22:00:07 +0200

    Update rev_print.c

--- Exam 02/00/rev_print/rev_print.c
+++ Exam 02/00/rev_print/rev_print.c
@@ -1,25 +1,31 @@
+#include <unistd.h>
+
+void    rev_print(char *s)
+{
+    int i = 0;
+
+    while (s[i])
+        i++;
+    i--;
+    while (s[i])
+    {
+        write(1, &s[i], 1);
+        i--;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 2)
+        rev_print(argv[1]);
+    write(1, "\n", 1);
+    return (0);
+}
+
 #include <stdio.h>
 #include <unistd.h>
 
-char	*rev(char *s)
-{
-	int	i = 0;
-	int	len = 0;
-	int	temp;
-	
-	while (s[len])
-		len++;
-	len--;
-	while (i < len)
-	{
-		temp = s[i];
-		s[i] = s[len];
-		s[len] = temp;
-		i++;
-		len--;
-	}
-	return (s);
-}
+
 
 /*int	main()
 {
