9c46aff8d567155a7ad85c318a24b240ed696e82
herib9 <157405740+herib9@users.noreply.github.com>
2025-07-11 14:52:50 +0200

    Update is_power_of_2.c

--- Exam 02/01/is_power_of_2/is_power_of_2.c
+++ Exam 02/01/is_power_of_2/is_power_of_2.c
@@ -15,6 +15,20 @@ int	    is_power_of_2(unsigned int n)
 	return (1);
 }
 
+int	    is_power_of_2(unsigned int n)
+{
+	if (n == 0)
+		return (0);
+	while (n / 2)
+	{
+		if (n % 2 == 0);
+			return (1);
+		else
+			return (0);
+	}
+	return (1);
+}
+
 int main(void)
 {
 	printf("%i\n", is_power_of_2(0));
