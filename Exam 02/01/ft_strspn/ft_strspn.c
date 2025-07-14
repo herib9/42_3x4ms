a102a6e63794810d46eda0ed8709c68e05e829b3
herib9 <157405740+herib9@users.noreply.github.com>
2025-07-15 00:54:23 +0200

    Update ft_strspn.c

--- Exam 02/01/ft_strspn/ft_strspn.c
+++ Exam 02/01/ft_strspn/ft_strspn.c
@@ -11,9 +11,10 @@ size_t	ft_strspn(const char *s, const char *accept)
 		j = 0;
 		while (accept[j])
 		{
-			if (accept[j] != s[i])		//if (accept[j] == s[i])
-				j++;				//break;
-		}					//j++;
+			if (accept[j] == s[i])
+				break;
+			j++;
+		}
 		if (!accept[j])
 			return (i);
 		i++;
