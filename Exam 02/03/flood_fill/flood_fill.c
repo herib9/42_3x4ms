d568a432815f8eca9e5264613c5e890520fd09ad
herib9 <157405740+herib9@users.noreply.github.com>
2025-07-14 19:12:43 +0200

    Update flood_fill.c

--- Exam 02/03/flood_fill/flood_fill.c
+++ Exam 02/03/flood_fill/flood_fill.c
@@ -3,37 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-void  flood_fill(char **tab, t_point size, t_point begin)
+void	fill(char **tab, t_point size, t_point cur, char to_fill)
 {
-	t_point p;
-	char c;
-	
-	c = tab[begin.y][begin.x];
-	tab[begin.y][begin.x] = 'F';
-	if (begin.y > 0 && tab[begin.y - 1][begin.x] == c)
-	{
-		p.x = begin.x;
-		p.y = begin.y - 1;
-		flood_fill(tab, size, p);
-	}
-	if ((begin.y < (size.y - 1)) && tab[begin.y + 1][begin.x] == c)
-	{
-		p.x = begin.x;
-		p.y = begin.y + 1;
-		flood_fill(tab, size, p);
-	}
-	if (begin.x > 0 && tab[begin.y][begin.x - 1] == c)
-	{
-		p.x = begin.x - 1;
-		p.y = begin.y;
-		flood_fill(tab, size, p);
-	}
-	if ((begin.x < (size.x - 1)) && tab[begin.y][begin.x + 1] == c)
-	{
-		p.x = begin.x + 1;
-		p.y = begin.y;
-		flood_fill(tab, size, p);
-	}
+	if (cur.y < 0 || cur.y >= size.y || cur.x < 0 || cur.x >= size.x || tab[cur.y][cur.x] != to_fill)
+		return ;
+	tab[cur.y][cur.x] = 'F';
+
+	fill(tab, size, (t_point){cur.x - 1, cur.y}, to_fill); 
+	fill(tab, size, (t_point){cur.x + 1, cur.y}, to_fill);
+	fill(tab, size, (t_point){cur.x, cur.y - 1}, to_fill);
+	fill(tab, size, (t_point){cur.x, cur.y + 1}, to_fill);
+}
+
+void	flood_fill(char **tab, t_point size, t_point begin)
+{
+	fill(tab, size, begin, tab[begin.y][begin.x]);
 }
 
 // Función auxiliar para imprimir el mapa
