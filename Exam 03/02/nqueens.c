6e95ab6a528ba93dcda3d85fe00a14663e6bb272
herib9 <157405740+herib9@users.noreply.github.com>
2025-11-14 22:58:27 +0100

    Refactor n-queens solution for clarity and efficiency

--- Exam 03/02/nqueens.c
+++ Exam 03/02/nqueens.c
@@ -1,34 +1,21 @@
-#include "stdio.h"
 #include "stdlib.h"
 #include "unistd.h"
 
-void ft_putnbr(int num)
+void ft_putnbr(int nb)
 {
-	if (num > 9)
-	{
-		ft_putnbr(num / 10);
-		char n = num % 10 + '0';
-		write(1, &n, 1);
-		return ;
-	}
-	else
-	{
-		char number = num + '0';
-		write(1, &number, 1);
-		return ;
-	}
+	if (nb > 9)
+		ft_putnbr(nb / 10);
+	write(1, &"0123456789"[nb % 10], 1);
 }
 
 int is_valid_pos(int *table, int x, int y)
 {
-	for (int i = 0; i < y; i++)
+	int i = 0;
+	while (i < y)
 	{
-		if (x == table[i])
+		if ((x == table[i]) || (x - y) == (table[i] - i) || (x + y) == (table[i] + i))
 			return (0);
-		if ((x - y) == (table[i] - i))
-			return (0);
-		if ((x + y) == (table[i] + i))
-			return (0);
+		i++;
 	}
 	return (1);
 }
@@ -38,38 +25,32 @@ void print_table(int *table, int size)
 	int i = 0;
 
 	while (i < size)
-	{
-		ft_putnbr(table[i]);
-		i++;
-	}
+		ft_putnbr(table[i++]);
 	write(1, "\n", 1);
 }
 
-void n_queen(int *table, int y, int x, int size)
+void n_queen(int *table, int y, int size)
 {
+	int x = 0;
 	if (y == size)
-	{
-		print_table(table, size);
-		return ;
-	}
+		return (print_table(table, size));
 	while(x < size)
 	{
 		if (is_valid_pos(table, x, y))
 		{
 			table[y] = x;
-			n_queen(table, y + 1, 0, size);
+			n_queen(table, y + 1, size);
 		}
 		x++;
 	}
-	return ;
 }
 
-int main(int ac, char **av)
+int main (int ac, char **av)
 {
 	if (ac < 2)
-		return (write(1, "\n", 1), 0);
+		return(write(1, "\n", 1), 0);
 	int size = atoi(av[1]);
 	int table[size];
-	n_queen(table, 0, 0, size);
+	n_queen(table, 0, size);
 	return (0);
 }
