7b12c03d8c4eb78f5b68ffd5327b7a93e09f7601
herib9 <157405740+herib9@users.noreply.github.com>
2026-03-06 22:26:41 +0100

    Add files via upload

--- Exam 04/Level_01/ft_popen/ft_popen (copy).c
+++ Exam 04/Level_01/ft_popen/ft_popen (copy).c
@@ -9,37 +9,21 @@ int ft_popen(const char *file, char *const argv[], char type)
 
 	if(!file || !argv ||(type != 'r' && type != 'w'))
 		return -1;
-
 	if(pipe(fd) == -1)
 		return -1;
-
-	pid = fork();
-	if (pid == -1)
-	{
-		close(fd[0]);
-		close(fd[1]);
-		return -1;
-	}
-
+	if((pid = fork()) == -1)
+		return (close(fd[0]), close(fd[1]), -1);
 	if(pid == 0)
 	{
-		if(type == 'r')
-		{
-			if(dup2(fd[1], STDOUT_FILENO) == -1)
-				exit(1);
-		}
-		else
-		{
-			if(dup2(fd[0], STDIN_FILENO) == -1)
-				exit(1);
-		}
+		if((type == 'r' && dup2(fd[1], STDOUT_FILENO) == -1) ||
+		   (type == 'w' && dup2(fd[0], STDIN_FILENO) == -1))
+			exit(1);
 		close(fd[0]);
 		close(fd[1]);
 		execvp(file, argv);
-		exit (1);
+		exit(1);
 	}
-	
-	if(type == 'r')
+	if (type == 'r')
 	{
 		close(fd[1]);
 		return(fd[0]);
